96d33f5dfb19c2eaed96f03d983a50188999951b
Cruz <ecruz981@uwsp.edu>
2022-11-20 21:09:49 -0600

    weird redirects

---
+++ CIS 341 Lab06/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CIS_341_Lab06.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+    /* This is used to link the Identity user account to the application user.
+     * Think of it as a cross-database foreign key.
+     */
+    public int UserAccountId { get; set; }
+}
+
--- CIS 341 Lab06/Program.cs
+++ CIS 341 Lab06/Program.cs
@@ -1,34 +1,47 @@
 using Microsoft.EntityFrameworkCore;
 using CIS_341_Lab06.Data;
+using Microsoft.AspNetCore.Identity;
+using CIS_341_Lab06.Areas.Identity.Data;
+using Microsoft.AspNetCore.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+
 
 builder.Services.AddDbContext<CommunityStoreContext>(options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'BookStoreContext' not found."))); ;
+/* IMPORTANT: You need to add the lines below manually after scaffolding in Identity if your
+ * project already has a database. The scaffolding logic is a little borked.
+ */
+builder.Services.AddDbContext<AuthenticationContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AuthenticationContextConnection") ?? throw new InvalidOperationException("Connection string 'BookStoreContext' not found.")));
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthenticationContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages(); // Also needs to be added
+
+/* Best practice: deny access by default by requiring an authenticated user to access any
+ * controller/action method is not specifically marked as AllowAnonymous 
+ */
+builder.Services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});
+
+// Modify authentication options. You can also change the user, signin and lockout settings.
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.SignIn.RequireConfirmedAccount = false;
+});
 
 var app = builder.Build();
 
-// Since the DbContext is a scoped service(?), we need to create a scope to retrieve the service.
-using (var scope = app.Services.CreateScope())
-{
-    // Service provider for the scope
-    var services = scope.ServiceProvider;
-    try
-    {
-        // Get the DbContext from the service provider
-        var context = services.GetRequiredService<CommunityStoreContext>();
-        DbInitializer.Initialize(context);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred creating the DB.");
-    }
-}
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -41,11 +54,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapRazorPages();
 app.Run();
